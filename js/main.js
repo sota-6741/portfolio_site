f302570079805f2723be11d36ba41506b1efda09
sota-6741 <32445070@fukuchiyama.ac.jp>
2025-10-22 19:23:31 +0900

    update: 初期ドラッグシミュレーション機能を削除

--- js/main.js
+++ js/main.js
@@ -9,31 +9,3 @@ if(!window.isDev) window.isDev = false;
 const webglMng = new WebGL({
     $wrapper: document.body
 });
-
-function simulateInitialDrag() {
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
-    const radius = 200; // 円運動の半径
-    const duration = 2000; // 円運動の持続時間（ミリ秒）
-    const startTime = performance.now();
-
-    function animate(currentTime) {
-        const elapsedTime = currentTime - startTime;
-        const progress = Math.min(elapsedTime / duration, 2); // Progressは0から2
-
-        const angle = progress * Math.PI * 2;
-
-        const x = centerX + radius * Math.cos(angle);
-        const y = centerY + radius * Math.sin(angle);
-
-        Mouse.setCoords(x, y);
-
-        if (progress < 2) {
-            requestAnimationFrame(animate);
-        }
-    }
-
-    requestAnimationFrame(animate);
-}
-
-simulateInitialDrag();
